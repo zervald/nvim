1bbe86842ac3419df364aef350dae54fe6dba584
Richard Demers <richardd1894@gmail.com>
2026-01-04 23:04:52 -0500

    obsidian config reset

--- lua/plugins/ui/snacks-image.lua
+++ lua/plugins/ui/snacks-image.lua
@@ -5,6 +5,12 @@ return {
     ---@class snacks.image.Config
     image = {
       enabled = true,
+      resolve = function(path, src)
+        local api = require 'obsidian.api'
+        if api.path_is_note(path) then
+          return api.resolve_attachment_path(src)
+        end
+      end,
       doc = {
         enabled = true,
         -- inline = false, -- default: true

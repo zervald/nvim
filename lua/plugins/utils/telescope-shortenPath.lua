d53bea834fb3b3549f91f34fb702f2d10f6f361a
Richard Demers <richard.demers@ionodes.com>
2026-02-18 14:21:48 -0500

    telescope filename

--- lua/plugins/utils/telescope-shortenPath.lua
+++ lua/plugins/utils/telescope-shortenPath.lua
@@ -1,10 +1,10 @@
 return {
-  "nvim-telescope/telescope.nvim",
+  'nvim-telescope/telescope.nvim',
   opts = {
     defaults = {
       path_display = {
-        -- "filename_first",
-        "truncate",
+        'filename_first',
+        'truncate',
         truncate = 2,
         -- shorten = { len = 3, exclude = { 2, -2, -1 } },
       },

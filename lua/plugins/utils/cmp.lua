d0b081e9b88ac0bc54a7079677cce4882e915969
Richard Demers <richardd1894@gmail.com>
2026-02-23 21:25:36 -0500

    cmp sorting

--- lua/plugins/utils/cmp.lua
+++ lua/plugins/utils/cmp.lua
@@ -1,14 +1,19 @@
 local cmp = require 'cmp'
 return {
   'hrsh7th/nvim-cmp',
+  dependencies = {
+    'lukas-reineke/cmp-under-comparator',
+  },
   opts = {
     sorting = {
       comparators = {
         cmp.config.compare.offset,
         cmp.config.compare.exact,
         cmp.config.compare.score,
-        cmp.config.compare.recently_used,
+        -- require('cmp-under-comparator').under,
         cmp.config.compare.kind,
+        cmp.config.compare.recently_used,
+        cmp.config.compare.locality,
         cmp.config.compare.sort_text,
         cmp.config.compare.length,
         cmp.config.compare.order,

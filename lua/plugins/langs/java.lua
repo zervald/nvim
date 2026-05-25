536d7d6f75a70d70ecd8966fe694944eb9840458
Richard Demers <richardd1894@gmail.com>
2026-05-25 15:55:13 -0400

    jdtls config

--- lua/plugins/langs/java.lua
+++ lua/plugins/langs/java.lua
@@ -1,1 +1,37 @@
-return {}
+return {
+  {
+    'neovim/nvim-lspconfig',
+    optional = true,
+    opts = {
+      servers = {
+        jdtls = {
+          handlers = {
+            ['$/progress'] = function(_) end,
+          },
+          format = {
+            settings = {
+              enabled = true,
+            },
+          },
+        },
+      },
+    },
+    {
+      'nvim-treesitter/nvim-treesitter',
+      optional = true,
+      opts = {
+        ensure_installed = { 'java' },
+      },
+    },
+    {
+      'stevearc/conform.nvim',
+      optional = true,
+      opts = {
+        -- formatters = { java = { -- prepend_args = { "-i", "2" }, }, },
+        formatters_by_ft = {
+          java = { 'google-java-format' },
+        },
+      },
+    },
+  },
+}

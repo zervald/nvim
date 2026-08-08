02e877e3e3297d93c5fe5d33213912245d1147cf
Richard Demers <richardd1894@gmail.com>
2026-08-08 11:02:39 -0400

    switch to blink.cmp

--- lua/plugins/utils/cmp.lua
+++ lua/plugins/utils/cmp.lua
@@ -1,23 +1,50 @@
-local cmp = require 'cmp'
 return {
-  'hrsh7th/nvim-cmp',
-  optional = true,
-  dependencies = {
-    'lukas-reineke/cmp-under-comparator',
+  {
+    'hrsh7th/nvim-cmp',
+    optional = true,
+    dependencies = {
+      'lukas-reineke/cmp-under-comparator',
+    },
+    opts = {
+      sorting = {
+        comparators = function()
+          local cmp = require 'cmp'
+          return {
+            cmp.config.compare.offset,
+            cmp.config.compare.exact,
+            cmp.config.compare.score,
+            -- require('cmp-under-comparator').under,
+            cmp.config.compare.kind,
+            cmp.config.compare.recently_used,
+            cmp.config.compare.locality,
+            cmp.config.compare.sort_text,
+            cmp.config.compare.length,
+            cmp.config.compare.order,
+          }
+        end,
+      },
+    },
   },
-  opts = {
-    sorting = {
-      comparators = {
-        cmp.config.compare.offset,
-        cmp.config.compare.exact,
-        cmp.config.compare.score,
-        -- require('cmp-under-comparator').under,
-        cmp.config.compare.kind,
-        cmp.config.compare.recently_used,
-        cmp.config.compare.locality,
-        cmp.config.compare.sort_text,
-        cmp.config.compare.length,
-        cmp.config.compare.order,
+  {
+    'saghen/blink.cmp',
+    optional = true,
+    opts = {
+      -- experimental signature help support
+      -- signature = { enabled = true },
+      keymap = {
+        preset = 'super-tab',
+        ['<Tab>'] = { 'select_next', 'snippet_forward', 'fallback' },
+        ['<S-Tab>'] = { 'select_prev', 'snippet_backward', 'fallback' },
+        ['<CR>'] = { 'select_and_accept', 'fallback' },
+        ['<S-CR>'] = {},
+      },
+      completion = {
+        list = {
+          selection = {
+            -- lazyvim default : false
+            preselect = false,
+          },
+        },
       },
     },
   },
